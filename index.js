d112e0c3ba5dcaa95b3386f5c6e096795c5c465c
Smid98 <bramdirks@live.nl>
2019-04-25 15:20:25 +0200

    Port changed

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var http = require('http');
+var PORT = 5000;
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello World!');
-}).listen(8080);
+}).listen(PORT);
